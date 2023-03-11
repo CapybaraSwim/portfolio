26ea44073f261240fa944a1be37c898c50e3ee20
CapybaraSwim <ilya-golovin-03@mail.ru>
2023-03-11 11:03:46 +0300

    added new block with video

--- js/js1.js
+++ js/js1.js
@@ -9,3 +9,12 @@ input.addEventListener('blur', function(){
 input.addEventListener('focus', function(){
 	form.classList.add('form--active');
 });
+// video
+const video = document.getElementById('video');
+const videoControls = document.getElementById('video-controls');
+
+const videoWorks = !!document.createElement('video').canPlayType;
+if (videoWorks) {
+  video.controls = false;
+  videoControls.classList.remove('hidden');
+}
