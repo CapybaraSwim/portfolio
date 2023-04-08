c4687c192f871bb87a20bbf310ba9bd748b91149
CapybaraSwim <ilya-golovin-03@mail.ru>
2023-04-08 09:39:10 +0300

    fixed any problems

--- js/js1.js
+++ js/js1.js
@@ -32,3 +32,8 @@ function arrowsActive() {
 
     active = this;
 }
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("burger").addEventListener("click", function(){
+    document.querySelector("header").classList.toggle("open")
+  })
+})
